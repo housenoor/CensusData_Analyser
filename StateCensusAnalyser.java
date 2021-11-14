d6a1b726b2b48834579841312d3795a8a6335f65
Srikanth <srikanthh994@gmail.com>
2021-11-14 23:40:28 +0530

    Added:To load State Code From CSV file

--- StateCensusAnalyser.java
+++ StateCensusAnalyser.java
@@ -1,30 +1,28 @@
 package com.indiancensusdataanalyser;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class StateCensusAnalyser {
+  
+    public void analyseIndianStateCode() throws FileNotFoundException {
+        String path = "C:\\Users\\admin\\eclipse-workspace\\JavaPractice\\src\\main\\java\\com\\indiancensusdataanalyser\\IndiaStateCode.CSV";
+        String line = "";
+        String splits = ",";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(splits);
+                System.out.println("Serial Number: " + data[0] + ", State Name: " + data[1] + ", TIN: " + data[2] + ",State Code: " + data[3]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void analyseCensusData() throws FileNotFoundException {
-		String path = "C:\\Users\\admin\\eclipse-workspace\\JavaPractice\\src\\main\\java\\com\\indiancensusdataanalyser\\IndianStateCensusData.CSV";
-		String line = "";
-		String splits = ",";
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-		try {
-			while ((line = bufferedReader.readLine()) != null) {
-				String[] data = line.split(splits);
-				System.out.println("State: " + data[0] + ", Population: " + data[1] + ", Area: " + data[2]
-						+ ", Density: " + data[3]);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(" Welcome to Indian State Census Analyser");
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println(" Welcome to Indian State Census Analyser");
-
-	}
+    }
 }
